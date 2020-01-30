a2ffd40e7114c7cfb3ca7bc5cafdd7ede098e98e
shweta R Pawar <45356409+ShwetaRPawar@users.noreply.github.com>
2020-01-30 22:40:28 +0530

    Update sampleDart.dart

--- sampleDart.dart
+++ sampleDart.dart
@@ -1,32 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main()
-{
+void main() {
   runApp
-   (
+  (
     MaterialApp
     (
-      debugShowCheckedModeBanner: false,
-      home: Scaffold
-      (
-          backgroundColor: Colors.blueGrey,
-          appBar: AppBar
-          (
-            title: Center(
-              child: Text('My First APP',style: TextStyle(
-                color: Colors.red),
-              ),
-              ),
-            backgroundColor: Colors.blueGrey[900]
-          ),
-          body: Center(
-            child: Image(
-              image:
-                    NetworkImage('https://images-na.ssl-images-amazon.com/images/I/61NhPi2PopL._AC_SX425_.jpg'),
-            ),
-          ),
-          
-      ),
-    ),
+      home: Center
+        (
+          child: Text("Hello Shweta"),
+        )
+    )
   );
 }
