cad1fb8ac0e8cf6995c541efe8ffffcc305c3c8c
ShwetaRPawar <pawarshweta1996@gmail.com>
2020-01-30 22:34:21 +0530

    Simple dart project

---
+++ sampleDart.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp
+   (
+    MaterialApp
+    (
+      debugShowCheckedModeBanner: false,
+      home: Scaffold
+      (
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar
+          (
+            title: Center(
+              child: Text('My First APP',style: TextStyle(
+                color: Colors.red),
+              ),
+              ),
+            backgroundColor: Colors.blueGrey[900]
+          ),
+          body: Center(
+            child: Image(
+              image:
+                    NetworkImage('https://images-na.ssl-images-amazon.com/images/I/61NhPi2PopL._AC_SX425_.jpg'),
+            ),
+          ),
+          
+      ),
+    ),
+  );
+}
